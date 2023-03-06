06dd6bdb5c69e9c0d7edbf65619822f76b2292df
zgs225 <zgs225@gmail.com>
2023-03-06 18:13:02 +0800

    add setup theme

--- misc/02-theme.sh
+++ misc/02-theme.sh
@@ -2,4 +2,21 @@
 
 . ./utils.sh
 
-# Setup terminal theme
+# Setup gnome terminal theme to gruvbox-material
+function setup_gnome_terminal_theme() {
+    # If gnome terminal is not installed, exit
+    if ! command -v gnome-terminal &> /dev/null; then
+        l_error "Gnome terminal is not installed"
+        return
+    fi
+
+    if [ -d "$HOME/.gogh" ]; then
+        l_skip "Gnome terminal theme is already installed"
+        return
+    fi
+
+    # Install gnome terminal theme
+    git clone https://github.com/Gogh-Co/Gogh.git "$HOME/.gogh"
+    bash "$HOME/.gogh/themes/gruvbox-material.sh"
+    l_success "Gnome terminal theme installed"
+}
