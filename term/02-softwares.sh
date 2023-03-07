36e2a734e1bd2d067afbf33341cf913569515edc
zgs225 <zgs225@gmail.com>
2023-03-07 19:00:55 +0800

    add rclone

--- term/02-softwares.sh
+++ term/02-softwares.sh
@@ -37,5 +37,6 @@ install_via_apt universal-ctags
 install_via_apt wireguard
 install_via_apt wireguard-tools
 install_via_apt resolvconf
+install_via_apt rclone
 
 install_prebuilt_zipbin bw "https://vault.bitwarden.com/download/?app=cli&platform=linux"
