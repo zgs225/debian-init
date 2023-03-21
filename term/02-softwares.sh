4a916a0e779e61e5b16d4e58b308373335a579a1
zgs225 <zgs225@gmail.com>
2023-03-21 09:59:04 +0800

    feat: add lm-sensors

--- term/02-softwares.sh
+++ term/02-softwares.sh
@@ -58,5 +58,6 @@ install_via_apt tesseract-ocr
 install_via_apt libtesseract-dev
 install_via_apt tesseract-ocr-chi-sim
 install_via_apt htop
+install_via_apt lm-sensors
 
 install_prebuilt_zipbin bw "https://vault.bitwarden.com/download/?app=cli&platform=linux"
