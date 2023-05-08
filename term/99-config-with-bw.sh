1f0048c4611145e29d46899e513b12213fbfca3c
zgs225 <zgs225@gmail.com>
2023-05-08 10:54:58 +0800

    feat: remove wireguad configration

--- term/99-config-with-bw.sh
+++ term/99-config-with-bw.sh
@@ -40,40 +40,6 @@ fi
 set_go_env GOPRIVATE  172.16.21.59
 set_go_env GOINSECURE 172.16.21.59
 
-# Wireguard configuration with bitwarden
-WIREGUARD_CLIENT_CONF="/etc/wireguard/wg0.conf"
-if sudo test -f "${WIREGUARD_CLIENT_CONF}"; then
-    l_skip "wireguard client configuration file is already exists."
-else
-    l_info "get wireguard client configuration file from bitwarden."
-    ITEM=$(bw get item "WireGuard Client LinuxMint")
-    PRIVATE_KEY=$(echo "${ITEM}" | jq -c '.login.password' | tr -d '"')
-    ENDPOINT=$(echo "${ITEM}" | jq -c '.fields[] | select (.name == "Endpoint") | .value' | tr -d '"')
-
-    TMPFILE=$(mktemp)
-    cat <<-EOF > "${TMPFILE}"
-    [Interface]
-    Address = 10.247.113.13/24, fd54:5612:3279:4624::13/64
-    DNS = 1.1.1.1, 2606:4700:4700::1111
-    PrivateKey = ${PRIVATE_KEY}
-    MTU = 1280
-
-    [Peer]
-    PublicKey = zWsKalVtEi5sMQdCjy6Y47M9wB48FnuXBCd8tfIMHWU=
-    PresharedKey = EzOpd0EAxVgiKRxDp1rHNC33Dj9vzlIpd2xmLYh7pNg=
-    AllowedIPs = 10.247.113.0/24, fd54:5612:3279:4624::/64
-    Endpoint = ${ENDPOINT}
-    PersistentKeepalive = 25
-EOF
-    sudo mv "${TMPFILE}" "${WIREGUARD_CLIENT_CONF}"
-    sudo chmod 0600 "${WIREGUARD_CLIENT_CONF}"
-
-    sudo systemctl enable wg-quick@wg0
-    sudo systemctl start wg-quick@wg0
-
-    l_success "wireguard client configuration file is configured."
-fi
-
 # Config ssh private key with bitwarden
 SSH_PRIVATE_KEY_FILE="${HOME}/.ssh/id_rsa"
 if test -f "${SSH_PRIVATE_KEY_FILE}"; then
